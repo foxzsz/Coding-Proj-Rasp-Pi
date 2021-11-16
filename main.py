e93c0f3563b192d83372e61a1d657bbd972d6707
foxzsz <82937328+foxzsz@users.noreply.github.com>
2021-11-16 09:52:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import json
-import random
 import time
 from datetime import datetime
-#from sensors import *
+from sensors import *
 import threading
 
 from flask import Flask, Response, render_template
 
 application = Flask(__name__)
-random.seed()  # Initialize the random number generator
 
 
 
@@ -22,13 +20,13 @@ def chart_data():
     def update_chart_data():
         while True:
             json_data = json.dumps(
-                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.randint(1,50)})
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': humidity})
             yield f"data:{json_data}\n\n"
-            time.sleep(1)
+            time.sleep(10)
 
     return Response(update_chart_data(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
-    #threading.Thread(target=HumidityUpdater).start()
+    threading.Thread(target=HumidityUpdater).start()
     application.run(debug=True, threaded=True)
--- sensors.py
+++ sensors.py
@@ -1,10 +1,33 @@
 import Adafruit_DHT
+import time
+
 
 humidity = 0
+count = 0
+humidityminute =  []
+temperatureminute = []
 
 
+def MinuteAverageCalculator():
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    DHT_PIN = 4
+    while True:
+        if count == 60:
+            humdityaverage = sum(humidityminute) / len(humidityminute)
+            temperatureaverage = sum(temperatureminute) / len(temperatureminute)
+            count = 0
+            minutearr.clear()
+            temperatureminute.clear()
+        else:
+            humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+            humidityminute.append(humidity)
+            temperatureminute.append(temperature)
+            count +=1
+            time.sleep(1)
 
-def HumidityUpdater():
+        
+
+""" def HumidityUpdater():
     global humidity
     try:
         DHT_SENSOR = Adafruit_DHT.DHT22
@@ -13,10 +36,8 @@ def HumidityUpdater():
         print("Error connecting to Humdity sensor")
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-    while True:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
+        if humidity is not None and temperature is not None:
+           print(" Updated values Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        else:
+           print("Failed to retrieve data from humidity sensor")
+        time.sleep(1) """
