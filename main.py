5c4dcad123088c6ef22cb6f4fc26fd8c3f5262b8
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-16 18:59:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import json
-import time
+import json, time, random
 from datetime import datetime
-from sensors import *
-import threading
-
+#from sensors import *
+#import threading
 from flask import Flask, Response, render_template
 
 application = Flask(__name__)
-
-
+random.seed()
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
-
 @application.route('/chart-data')
 def chart_data():
     def update_chart_data():
-        while True:
+        ls_counter = 0
+        while ls_counter <= 20:
             json_data = json.dumps(
-                {'time': datetime.now().strftime('%H:%M:%S'), 'value': humidity})
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': random.randint(0, 50)})
             yield f"data:{json_data}\n\n"
-            time.sleep(10)
+            """
+            json_data_humidity = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': humidityminute[ls_counter]})
+            yield f"data:{json_data}\n\n"
+            json_data_temperature = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': temperatureminute[ls_counter]})
+            yield f"data:{json_data}\n\n"
+            """
+            ls_counter += 1
+            time.sleep(1)
 
     return Response(update_chart_data(), mimetype='text/event-stream')
 
-
-if __name__ == '__main__':
-    threading.Thread(target=HumidityUpdater).start()
-    application.run(debug=True, threaded=True)
+#threading.Thread(target=MinuteAverageCalculator).start()
+application.run(debug=True, threaded=True)
