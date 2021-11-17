a66f2cdc79b0ff26fcccd9521f6cc28b99c1ab00
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-17 13:45:17 +0100

    Add files via upload

--- sensors.py
+++ sensors.py
@@ -1,19 +1,20 @@
 import Adafruit_DHT, time, board
 
 count = 0
-humidityMinute =  []
+humidityMinute = []
 temperatureMinute = []
+
 
 def MinuteAverageCalculator():
     DHT_SENSOR = Adafruit_DHT.DHT22(board.D4)
-    #DHT_PIN = 4
-    global count, humidityAverage, temperatureAverage
-    while count<=20:
-        #humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    # DHT_PIN = 4
+    global count, humidityAverage, temperatureAverage, humidityMinute, temperatureMinute
+    while count <= 20:
+        # humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
         humidity, temperature = DHT_SENSOR.humidity, DHT_SENSOR.temperature
         humidityMinute.append(humidity)
         temperatureMinute.append(temperature)
-        count +=1
+        count += 1
         time.sleep(1)
         humidityAverage = sum(humidityMinute) / len(humidityMinute)
         temperatureAverage = sum(temperatureMinute) / len(temperatureMinute)
