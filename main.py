60d17512d2a4c1b1ffaffc1fc5d9a3613e97f25d
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-20 20:26:55 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json, time, random
+import json, time, random, threading
 from datetime import datetime
-#from sensors import *
-#import threading
+from sensors import *
 from flask import Flask, Response, render_template
 
 application = Flask(__name__)
@@ -14,19 +13,22 @@ def index():
 @application.route('/chart-data')
 def chart_data():
     def update_chart_data():
-        #ls_counter = 0
+        ls_counter = 0
         while True:
+            equation = "Equation: Humidity = Temperature × " + str(humidityAverage/ temperatureAverage)
+            """
             json_data = json.dumps(
                 {'time': datetime.now().strftime('%H:%M:%S'), 'humidity': random.randint(1, 50), 'temperature': random.randint(1, 50)})
             yield f"data:{json_data}\n\n"
             """
             json_data = json.dumps(
-                {'time': datetime.now().strftime('%H:%M:%S'), 'humidity': humidity_data[ls_counter], 'temperature': temperature_data[ls_counter]})
+                {'time': datetime.now().strftime('%H:%M:%S'), 'humidity': humidity_data[ls_counter], 'temperature': temperature_data[ls_counter], 'equation': equation})
             yield f"data:{json_data}\n\n"
-            """
+            ls_counter += 1
+            
             time.sleep(1)
 
     return Response(update_chart_data(), mimetype='text/event-stream')
 
-#threading.Thread(target=data).start()
+threading.Thread(target=data).start()
 application.run(debug=True, threaded=True)
