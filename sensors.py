ba135093fb08a62722e1d0d519cd0fc026d19c10
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-20 20:27:30 +0100

    Add files via upload

--- sensors.py
+++ sensors.py
@@ -1,14 +1,17 @@
-import Adafruit_DHT, time
+import adafruit_dht, time
+from board import *
 
 humidity_data = []
 temperature_data = []
 
 def data():
     global humidity_data, temperature_data, humidityAverage, temperatureAverage
+    dht22 = adafruit_dht.DHT22(D4, use_pulseio=False)
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-        humidity_data.append(round(humidity, 2))
-        temperature_data.append(round(temperature, 2))
+        humidity  = round(dht22.humidity, 2)
+        temperature = round(dht22.temperature, 2)
+        humidity_data.append(humidity)
+        temperature_data.append(temperature)
         humidityAverage = sum(humidity_data) / len(humidity_data)
         temperatureAverage = sum(temperature_data) / len(temperature_data)
-        time.sleep(1) 
+        time.sleep(1)
