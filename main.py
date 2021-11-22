75751b9ac6bc70ac0cfdef099cfec3916f03994e
foxzsz <82937328+foxzsz@users.noreply.github.com>
2021-11-22 18:29:04 +0100

    Add files via upload
    very nice

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import json, time, threading
 # import random
+import sys,os
 from datetime import datetime
-from sensors import *
 from flask import Flask, Response, render_template
 
 application = Flask(__name__)
+
+import adafruit_dht, time
+from board import *
+
+humidity_data = []
+temperature_data = []
+humidityAverage = 0
+temperatureAverage = 0
+SENSOR_PIN = D4
+dht22 = adafruit_dht.DHT22(SENSOR_PIN, use_pulseio=False)
+
+def data():
+    global humidity_data, temperature_data, humidityAverage, temperatureAverage, dht22
+    while True:
+        try:
+            humidity  = dht22.humidity
+            temperature = dht22.temperature
+            print(temperature)
+            print(humidity)
+            if temperature == None:
+                print("Unable to get temperature")
+            else:
+                temperature_data.append(temperature)
+                temperatureAverage = sum(temperature_data) / len (temperature_data)
+            if humidity == None:
+                print("Unable to get humidity")
+            else:
+                humidity_data.append(humidity)
+                humidityAverage = sum(humidity_data) / len (humidity_data)
+            time.sleep(1)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(f"Ignored error: {e}")
+            time.sleep(1)
+            continue
 
 
 # random.seed()
@@ -17,6 +54,7 @@ def index():
 @application.route('/chart-data')
 def chart_data():
     def update_chart_data():
+        global humidityAverage,temperatureAverage,humidity_data
         while True:
             equation = "Equation: Humidity = Temperature × " + str(humidityAverage / temperatureAverage)
             """
@@ -31,6 +69,8 @@ def chart_data():
 
     return Response(update_chart_data(), mimetype='text/event-stream')
 
-
 threading.Thread(target=data).start()
-application.run(debug=True, threaded=True)
+application.run(host="0.0.0.0",debug=True, threaded=True)
+
+
+
