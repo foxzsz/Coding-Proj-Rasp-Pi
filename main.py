20a72af60858588e3bf9c5876265f007c4460f69
foxzsz <82937328+foxzsz@users.noreply.github.com>
2021-11-24 19:39:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from displays import *
+from .displays import *
 import templates.tk as tk
 
+## Total lines of code: 248
+
+
+## This will start a thread / subproccess which will update the data every second, this is required for thw website and the graph
 threading.Thread(target=data).start()
 
+
+## Create a new Tkinter window and use the functions as the paramaters
 window = tk.win(mat, web)
