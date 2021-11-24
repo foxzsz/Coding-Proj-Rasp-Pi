6c14ec2f854661774d52d4a0310ef024648cc36e
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-24 22:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from .displays import *
+from displays import mat_plt, web_graph
 import templates.tk as tk
 
 ## Run the class window from tk file and pass the commands mat(which is used for graphing on matplotlib) and web(which is used to graph on a webpage) to the buttons of the window
-window = tk.win(mat, web)
+window = tk.win(mat_plt.mat, web_graph.web)
