ceaf12fc55f0f441b1d452f26c7867deb05e6f04
Sridhar <62298338+12-sridhar@users.noreply.github.com>
2021-11-25 20:31:37 +0100

    Add files via upload

---
+++ mat_plt.py
@@ -0,0 +1,29 @@
+# Importing the needed modules
+import threading, matplotlib.pyplot as plt, itertools as it
+from matplotlib.animation import FuncAnimation
+from sensors import *
+
+# Matplotlib graph function
+def mat():
+    # Threading is used to run many functions and loops at a time. This data function is used from sensors.py
+    threading.Thread(target=data).start()
+    time_values = []
+    index = it.count()
+
+    def animate(i):
+        # Plotting a line graph using the values from sensors.py
+        time_values.append(next(index))
+        plt.plot(time_values, humidity_data, '-b', label='humidity')
+        plt.plot(time_values, temperature_data, '-r',  label='temperature')
+        # Creating a scatter plot so people can know the data values
+        plt.scatter(time_values, humidity_data, c='blue')
+        plt.scatter(time_values, temperature_data, c= 'red')
+        # The legend location in the window is upper left
+        plt.gca().legend(('Humidity', 'Temperature'), loc="upper left")
+    # Animate the graph using funcanimation and set the interval as 1000ms(milliseconds)
+    live = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+    plt.xlabel("time(seconds)")
+    plt.ylabel("humidity(%), temperatre(°C)")
+    # Display the window
+    plt.show()
